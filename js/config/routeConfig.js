5b3fe05d13d61f8026416d81c1cf1f9b1a48a58e
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-04-08 21:19:42 -0300

    08/04/2017

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -1,7 +1,15 @@
 angular.module("listaTelefonica").config(function($routeProvider){
     $routeProvider.when("/contatos", {
         templateUrl: "view/contatos.html",
-        controller: "listaTelefonicaController"
+        controller: "contatosController",
+        resolve:{
+            contatos: function(contatosAPI){
+                return contatosAPI.getContatos();
+            },
+            operadoras: function(operadorasAPI){
+                return operadorasAPI.getOperadoras();
+            }   
+        }
     });
     $routeProvider.when("/novoContato",{
         templateUrl: "view/novoContato.html",
---
+++ js/controllers/contatosController.js
@@ -0,0 +1,40 @@
+angular.module("listaTelefonica").controller("contatosController",function($scope,contatos,operadoras,serialGenerator){
+				$scope.app = "Lista Telefonica";
+				$scope.contatos = contatos.data;
+				$scope.operadoras = operadoras.data;
+
+				
+				var generateSerial  = function (contatos) {
+						contatos.forEach(function(item){
+							item.serial = serialGenerator.generate();
+						});
+				};
+
+				$scope.apagarContatos = function(contatos){
+					 contatos.filter(function(contato){
+						if(contato.selecionado)
+							contatosAPI.deleteContato(contato).success(function(data){
+							$scope.contatos = data;
+						});
+						
+					});
+	
+					/*$scope.contatos = contatos.filter(function (contato){
+							if(!contato.selecionado) return contato;
+					});*/
+
+				};
+
+				$scope.isContatoSelecionado = function (contatos){
+					return contatos.some(function(contato){
+						return contato.selecionado;
+					});
+				};
+				$scope.ordenarPor = function(campo){
+					$scope.criterioDeOrdenacao = campo;
+					$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+				}
+				
+				generateSerial($scope.contatos);
+				
+			});
