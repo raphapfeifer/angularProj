d9804e1d44565899a0705a63d5096c3e41116094
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-05-18 22:58:08 -0300

    18/05/2017

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var cleanCSS = require('gulp-clean-css');
 var runSequence = require('run-sequence');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
-//var livereload = require('gulp-livereload');
+//var exec = require('child_process').exec;
+var browserSync = require('browser-sync').create();
 
 gulp.task('clean',function(){
     return gulp.src('dist/')
@@ -35,34 +36,57 @@ gulp.task('htmlmin', function(){
     return gulp.src('view/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist/view'))
+    .pipe(browserSync.reload({
+            stream: true
+    }));
 });
 
 gulp.task('cssmin',function(){
     return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css','css/**/*.css'])
     .pipe(cleanCSS())
     .pipe(concat('styles.min.css'))
-    .pipe(gulp.dest('dist/css'));  
+    .pipe(gulp.dest('dist/css')) 
+    .pipe(browserSync.reload({
+            stream: true
+    })); 
 });
 
 gulp.task('imagemin',function(){
     return gulp.src('img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest('dist/img'))
+    .pipe(browserSync.reload({
+            stream: true
+    }));
 });
+
 
 gulp.task('copy',function(){
     return gulp.src('ListaTelefonica-prod.html')
-    .pipe(rename('ListaTelefonica.html'))
+    .pipe(rename('index.html'))
     .pipe(gulp.dest('dist/'))
-})
+});
 
-gulp.task('default',function(cb){
-   return runSequence('clean',['jshint','uglify','htmlmin','cssmin','imagemin','copy'],cb)
+/*gulp.task('server', function (cb) {
+  exec('node/server.js', function (err, stdout, stderr) {
+    cb(err);
+  });
+});*/
+
+gulp.task('browser-sync',function(){
+    browserSync.init(null,{
+        open: false,
+        server: {
+            baseDir:'dist'
+        }
+    });
 });
 
 
-    //exemplo de server
-    /*livereload.listen();
-    gulp.watch(cssDir,function(){
-        gulp.src(cssDir).pipe(liveredload());
-    });*/
+gulp.task('default',function(cb){
+   return runSequence('clean',['jshint','uglify','htmlmin','cssmin','imagemin','copy','browser-sync'],cb)
+});
+
+
+
+
